46e5426e5fd1e058b898fd142bdff302707bc5e0
Minh Le <leminh9844@gmail.com>
2023-10-20 10:39:28 -0400

    add activity and instructions

--- app/src/main/java/com/example/multigame/gameBasic/sudoku/SudokuGameActivity.java
+++ app/src/main/java/com/example/multigame/gameBasic/sudoku/SudokuGameActivity.java
@@ -1,19 +1,77 @@
 package com.example.multigame.gameBasic.sudoku;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.FragmentNavigator;
+
 import com.example.multigame.R;
-import android.content.Context;
-import android.media.MediaPlayer;
-import android.os.Bundle;
-import android.os.Handler;
+import com.example.multigame.base.BaseActivity;
+import com.example.multigame.base.DialogInstruction;
+import com.example.multigame.databinding.ActivitySudokuGameBinding;
 
-public class SudokuGameActivity extends AppCompatActivity {
+public class SudokuGameActivity extends BaseActivity<ActivitySudokuGameBinding> {
+
+    private Menu menuList;
+
+    @Override
+    protected boolean isHaveRightMenu() {
+        return true;
+    }
+
+    @Override
+    protected boolean isHaveBackMenu() {
+        return true;
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_sudoku_game;
+    }
+
+    @Override
+    protected String getActionBarTitle() {
+        return getString(R.string.sudoku_game_title);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sudoku_game);
+    }
+
+    @Override
+    protected void subscribeUi() {
+        binding.setAction(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menuList = menu;
+        getMenuInflater().inflate(R.menu.menu_app, menu);
+        menuList.findItem(R.id.menu_information).setVisible(false);
+        return isHaveRightMenu();
+    }
+
+    public void showMenuRight(boolean isShow){
+        menuList.findItem(R.id.menu_information).setVisible(isShow);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_information:
+                DialogInstruction dialogInstruction = DialogInstruction.newInstance(R.layout.dialog_instruction_sudoku);
+                dialogInstruction.show(getSupportFragmentManager(),"instruction");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
