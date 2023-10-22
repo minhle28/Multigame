56a4b40ed7d1f5ebc9a1c4c1cf7ebe70d44270e6
Minh Le <leminh9844@gmail.com>
2023-10-21 22:35:50 -0400

    add game play fragment

---
+++ app/src/main/java/com/example/multigame/gameBasic/sudoku/SudokuGamePlayFragment.java
@@ -0,0 +1,14 @@
+package com.example.multigame.gameBasic.sudoku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SudokuGamePlayFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sudoku_game_play_fragment);
+    }
+}
